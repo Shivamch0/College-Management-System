d3a89ccce6deaf5981b8766667591713ca4105df
Shivam <shivamch0865@gmail.com>
2025-10-27 18:22:57 +0530

    cors logic updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,10 +5,20 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-    origin : process.env.CORS_ORIGIN,
-    credentials : true
-}));
+const allowedOrigins = process.env.CORS_ORIGIN.split(",");
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json({limit : "16kb"}));
 app.use(express.urlencoded({extended : true , limit : "16kb"}));
